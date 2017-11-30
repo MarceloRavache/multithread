40126736b6310fc809bbcb9526feefdb9ee4e57c
MarceloRavache <32018314+MarceloRavache@users.noreply.github.com>
2017-11-30 00:22:34 -0300

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -2,106 +2,111 @@
 #include <stdlib.h>
 #include <string.h>
 
-float* cm(int tam){
-	int i;
-	int aux = tam*tam;
-	float *matriz = (float *)malloc(aux*sizeof(float));
-	return matriz;
-}
+float* criarmatriz(int tamanho);
 
 int main(int argc, char *argv[])
 {
-
-	char c[100],*mtA=argv[1],*mtB=argv[2],*mtC=argv[3];
+	char matrizA=argv[1],matrizB=argv[2],matrizC=argv[3];
 	char buffer[10000];
-	char ch;
-	char bf[10];
+	char caractere;
+	char minibuffer[10];
 	int i=0,j=0;
 
-	FILE *arq1 = fopen(mtA,"r");
-	FILE *arq2 = fopen(mtB,"r");
+	FILE *arquivo1 = fopen(matrizA,"r");
+	FILE *arquivo2 = fopen(matrizB,"r");
 
-	int tam1,tam2;
+	int tamanho1,tamanho2;
 
 	fgets(buffer,sizeof(buffer),arq1);
-	tam1 = atoi(buffer);
+	tamanho1 = atoi(buffer);
 
 	fgets(buffer,sizeof(buffer),arq2);
-	tam2 = atoi(buffer);
+	tamanho2 = atoi(buffer);
 
-	if(tam1 != tam2){
+	if(tamanho1 != tamanho2){
 		printf("\nAs duas matrizes não são do mesmo tamanho\n");
 	}else{
 
 	strcpy(buffer,"");
 	
-	float *matrizA = cm(tam1);
+	float *matriz1 = criarmatriz(tamanho1);
 	
 	//Pega todos os valores do txt1 e coverte em uma matriz digital
 
-	while((ch=fgetc(arq1)) != EOF){
+	while((caractere=fgetc(arquivo1)) != EOF){
  		if(ch == ':'){
- 			matrizA[i] = atof(buffer);
+ 			matriz1[i][j] = atof(buffer);
  			strcpy(buffer,"");
- 			i++;
+ 			j++;
  		}else if(ch == '\n'){
  			matrizA[i] = atof(buffer);
  			strcpy(buffer,"");
  			i++;
+			j=0;
  		}else{
- 			bf[0]=ch;
+ 			minibuffer[0]=caractere;
  			strcat(buffer,bf);
  		}
  	}
 	
-	float *matrizB = cm(tam2);
+	float *matriz2 = criarmatriz(tam2);
 
 	i=0;
 	strcpy(buffer,"");
 
 	//Pega todos os valores do txt2 e coverte em uma matriz digital
 
-	while((ch=fgetc(arq2)) != EOF){
+	while((caractere=fgetc(arquivo2)) != EOF){
  		if(ch == ':'){
- 			matrizB[i] = atof(buffer);
+ 			matriz2[i][j] = atof(buffer);
  			strcpy(buffer,"");
-			i++;
+			j++;
  		}else if(ch == '\n'){
- 			matrizB[i] = atof(buffer);
+ 			matriz2[i][j] = atof(buffer);
  			strcpy(buffer,"");
 			i++;
+			j=0;
  		}else{
- 			bf[0]=ch;
+ 			minibuffer[0]=caractere;
  			strcat(buffer,bf);
  		}
  	}
 	
 	float acumulador;
 	int k;
-	float *matrizC = cm(tam1);
-	for(i=0;i<tam1;i++){
-		for(j=0;j<tam1;j++){
+	float *matriz1 = criarmatriz(tamanho1);
+	for(i=0;i<tamanho1;i++){
+		for(j=0;j<tamanho1;j++){
 			acumulador = 0;
-			for(k=0;k<tam1;k++){
-				acumulador = acumulador + (matrizA[(i*tam1)+k]*matrizB[(k*tam1)+j]);
+			for(k=0;k<tamanho1;k++){
+				acumulador = acumulador + (matriz1[i][k]*matriz2[k][j]);
 			}
-			matrizC[(i*tam1)+j] = acumulador;
+			matriz3[i][j] = acumulador;
 		}
 	}
 
-	FILE *arq3 = fopen(mtC,"w+");
-	fprintf(arq3, "%d\n",tam1);
+	FILE *arquivo3 = fopen(matrizC,"w+");
+	fprintf(arquivo3, "%d\n",tamanho1);
 
-	for (int i = 0; i < tam1; i++) {
-		for (int j = 0; j < tam1; j++) {
-			if (j < tam1 - 1)fprintf(arq3, "%.1f:", matrizC[(i*tam1)+j]);
-	 else fprintf(arq3, "%.1f", matrizC[(i*tam1)+j]);
+	for (int i = 0; i < tamanho1; i++) {
+		for (int j = 0; j < tamanho1; j++) {
+			if (j < tamanho1 - 1)fprintf(arquivo3, "%.1f:", matriz3[i][j]);
+	 else fprintf(arq3, "%.1f", matrizC[i][j]);
 	}
-	fprintf(arq3, "\n");
+	fprintf(arquivo3, "\n");
 	}
-	fclose(arq3);
+	fclose(arquivo3);
 }
 
-	fclose(arq1);
- 	fclose(arq2);
+	fclose(arquivo1);
+ 	fclose(arquivo2);
+}
+
+float* criarmatriz(int tamanho){
+	int i;
+	float **matriz = (float **)malloc(tamanho*sizeof(float *));
+	for(i=0;i<tamanho;i++){
+		matriz[i]= (float *) malloc(tamanho*sizeof(float));
+	}
+	return matriz;
 }
